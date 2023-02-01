e755fb72d7a7895099c92cc89e3085fcf8bc7aca
AnveeMehrotra <87015902+AnveeMehrotra@users.noreply.github.com>
2023-02-01 19:25:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function prevention(){
+    window.location="prevention.html";
+}
+
+function types(){
+    window.location="index.html";
+}
+
+
+function facts(){
+    window.location="facts.html";
+}
